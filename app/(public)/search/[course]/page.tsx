8b4fddc6b501233a25fe9437d8d9da03530f9e26
gihonhon <agunggihon@gmail.com>
2023-11-27 14:54:04 +0800

    fix grid card

--- app/(public)/search/[course]/page.tsx
+++ app/(public)/search/[course]/page.tsx
@@ -16,7 +16,6 @@ const CourseSearch = async ({ params }: { params: { course: string } }) => {
       <div className="mx-6 mt-4 mb-8 py-2 px-6 w-auto h-auto">
         <CoursesList title={params.course} />
       </div>
-      <Footer />
     </>
   );
 };
